8b21374222c260d0fbcd45fbfb47dfb447594b7f
ArturBK <artur.koefender@ilegra.com>
2015-09-23 23:45:01 -0300

    .

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -1,6 +1,29 @@
 (ns forca.core
   (:gen-class))
 
+(def total-de-vidas 6)
+
+(defn perdeu [] (print "você perdeu"))
+(defn ganhou [] (print "voce ganhou"))
+
+(defn letras-faltantes [palavra acertos]
+	(remove (fn [letra] (contains? acertos (str letra))) palavra))
+
+(defn acertou-a-palavra-toda? [palavra acertos]
+	(empty? (letras-faltantes palavra acertos))
+)
+
+
+(defn jogo [vidas palavra acertos]
+	(if (= vidas 0) 
+		(perdeu)
+		(if (acertou-a-palavra-toda? palavra acertos)
+			(ganhou)
+			(print "Chuta")
+		)
+	)
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
