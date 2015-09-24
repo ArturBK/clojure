adc4d4d7b7fbfde243f411141dec2774056e35d9
ArturBK <artur.koefender@ilegra.com>
2015-09-24 15:32:20 -0300

    .

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -13,16 +13,36 @@
 	(empty? (letras-faltantes palavra acertos))
 )
 
+(defn acertou? [chute palavra] (.contains palavra chute))
+
+(defn le-letra! [] (read-line))
+
+(defn imprime-forca [vidas palavra acertos]
+    (println "Vidas " vidas)
+    (map (fn [letra] (if (contains? acertos (str letra))
+        (print letra " ") (print "_" " "))) palavra)
+    (println))
+
+ (defn imprime-forca2 [vidas palavra acertos]
+    (println "Vidas " vidas)
+    (doseq [letra (seq palavra)]
+        (if (contains? acertos (str letra))
+            (print letra " ") (print "_" " ")))
+    (println))
 
 (defn jogo [vidas palavra acertos]
-	(if (= vidas 0) 
-		(perdeu)
-		(if (acertou-a-palavra-toda? palavra acertos)
-			(ganhou)
-			(print "Chuta")
-		)
-	)
-)
+    (cond
+        (= vidas 0) (perdeu)
+        (acertou-a-palavra-toda? palavra acertos) (ganhou)
+        :else   
+        (let [chute (le-letra!)]
+            (if (acertou? chute palavra)
+                (do
+                    (println "Acertou a letra!")
+                    (recur vidas palavra (conj acertos chute)))
+                (do
+                    (println "Errou a letra! Perdeu vida!")
+                    (recur (dec vidas) palavra acertos))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
